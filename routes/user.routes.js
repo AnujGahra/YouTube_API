77c1278f1eabb726f23e22a94c41fb609d94dc3f
AnujGahra <anujgahra244304@gmail.com>
2025-03-18 21:49:43 +0530

    add loginRoute

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import cloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 import mongoose from "mongoose";
 
 const router = express.Router();
 
+// SignUp routes
 router.post("/signup", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -30,7 +33,61 @@ router.post("/signup", async (req, res) => {
     res.status(201).json({
       user,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "something went wrong", message: error.message });
+  }
+});
+
+// Login routes
+router.post("/login", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isValid = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
+
+    if (!isValid) {
+      return res.status(500).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: existingUser._id,
+        channelName: existingUser.channelName,
+        email: existingUser.email,
+        phone: existingUser.phone,
+        logoId: existingUser.logoId,
+      },
+      process.env.JWT_TOKEN,
+      { expiresIn: "10d" }
+    );
+
+    res.status(200).json({
+      _id: existingUser._id,
+      channelName: existingUser.email,
+      email: existingUser.email,
+      phone: existingUser.phone,
+      logoId: existingUser.logoId,
+      logoUrl: existingUser.logoUrl,
+      token: token,
+      subscribers: existingUser.subscribers,
+      subscribedChannels: existingUser.subscribedChannels
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "something went wrong", message: error.message });
+  }
 });
 
 export default router;
