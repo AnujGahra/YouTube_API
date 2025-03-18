b10229c84844ecf6fd2c3db61abf25b6b28f6dce
AnujGahra <anujgahra244304@gmail.com>
2025-03-18 22:56:38 +0530

    add videoRoutes

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -4,11 +4,99 @@ import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import Video from "../models/video.model.js";
 import cloudinary from "../config/cloudinary.js";
+import { checkAuth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/upload", async (req, res) => {
+// Upload Video
+router.post("/upload", checkAuth, async (req, res) => {
   try {
+    const { title, description, category, tags } = req.body;
+    if (!req.files || !req.files.video || !req.files.thumbnail) {
+      return res
+        .status(400)
+        .json({ error: "Video and thumbnail are required" });
+    }
+
+    const videoUpload = await cloudinary.uploader.upload(
+      req.files.video.tempFilePath,
+      {
+        resource_type: "video",
+        folder: "videos",
+      }
+    );
+
+    const thumbnailUpload = await cloudinary.uploader.upload(
+      req.files.thumbnail.tempFilePath,
+      {
+        folder: "thumbnails",
+      }
+    );
+
+    const newVideo = new Video({
+      _id: new mongoose.Types.ObjectId(),
+      title,
+      description,
+      user_id: req.user_id,
+      videoUrl: videoUpload.secure_url,
+      videoId: videoUpload.public_id,
+      thumbnailUrl: thumbnailUpload.secure_url,
+      thumbnailId: thumbnailUpload.public_id,
+      category,
+      tags: tags ? tags.split(",") : [],
+    });
+
+    await newVideo.save();
+
+    res
+      .status(201)
+      .json({ message: "Video Upload Successfully", video: newVideo });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "Something went wrong", message: error.message });
+  }
+});
+
+// Update Video (no video change, Only metadata & thumbnail)
+router.put("/update/:id", checkAuth, async (req, res) => {
+  try {
+    const { title, description, category, tags } = req.body;
+    const videoId = req.params.id;
+
+    // find video by Id
+    let video = await Video.findById(videoId);
+    if (!video) {
+      return res.status(404).json({ error: "Video not found" });
+    }
+
+    if (video.user_id.toString() !== req.user_id.toString()) {
+      return res.status(403).json({ error: "unauthorized" });
+    }
+
+    if (req.files && req.files.thumbnail) {
+      await cloudinary.uploader.destroy(video.thumbnailId);
+
+      const thumbnailUpload = await cloudinary.uploader.upload(
+        req.files.thumbnail.tempFilePath,
+        {
+          folder: "thumbnail",
+        }
+      );
+
+      video.thumbnailUrl = thumbnailUpload.secure_url;
+      video.thumbnailId = thumbnailUpload.public_id;
+    }
+
+    // Update fields
+    video.title = title || video.title;
+    video.description = description || video.description;
+    video.category = category || video.category;
+    video.tags = tags ? tags.split(",") : video.tags;
+
+    await video.save();
+    res.status(200).json({ message: "Video updated successfully", video });
   } catch (error) {
     console.log(error);
     res
